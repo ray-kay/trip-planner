81a50b4ccdd73bf4399ff0cc339972baadc72c04
Ralf Hauptmann <ralf.hauptmann84@gmail.com>
2018-04-23 19:07:26 +1000

    chore: add basic module and component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { environment } from '../environments/environment';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { TripModule } from './trip/trip.module';
+import { UserModule } from './user/user.module';
+import { HomepageComponent } from './homepage/homepage.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    AppRoutingModule,
+    // feature modules
+    CoreModule,
+    SharedModule,
+    TripModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/trip/trip.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TripComponent } from './trip.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [TripComponent]
+})
+export class TripModule { }
